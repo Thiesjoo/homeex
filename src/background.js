cf9a7adc17ffbe6d1cf98f53be8c11d663cd078a
Thies Nieborg <git@thies.dev>
2022-05-07 19:53:08 +0200

    Fixed issue with inactive service worker

--- src/background.js
+++ src/background.js
@@ -11,29 +11,33 @@ function requestData(url, token) {
 }
 
 
-chrome.webRequest.onSendHeaders.addListener(
-  async function (details) {
-    const correctHeader = details.requestHeaders.find(x => x.name.toLowerCase() === "authorization")
+chrome.webNavigation.onBeforeNavigate.addListener(function () {
+  chrome.webRequest.onSendHeaders.addListener(
+    async function (details) {
+      const correctHeader = details.requestHeaders.find(x => x.name.toLowerCase() === "authorization")
 
-    if (correctHeader) {
-      const token = correctHeader.value.split(" ")?.[1];
-      if (!token) return;
+      if (correctHeader) {
+        const token = correctHeader.value.split(" ")?.[1];
+        if (!token) return;
 
-      try {
-        // Real server has to refresh
-        await fetch("https://auth.thies.dev/auth/refresh/access", {
-          credentials: "include",
-          method: "GET",
-        })
-        requestData("https://auth.thies.dev/api/providers/me/via/pos", token)
-      } catch (e) {
-        console.error("Request to thies.dev failed with error:", e)
+        try {
+          // Real server has to refresh
+          await fetch("https://auth.thies.dev/auth/refresh/access", {
+            credentials: "include",
+            method: "GET",
+          })
+          requestData("https://auth.thies.dev/api/providers/me/via/pos", token)
+        } catch (e) {
+          console.error("Request to thies.dev failed with error:", e)
+        }
+
+      } else {
+        console.error("How can you have no auth header, but send request to POS")
       }
-
-    } else {
-      console.error("How can you have no auth header, but send request to POS")
-    }
-  },
-  { urls: ["https://pos.svia.nl/api/account/"] },
-  ["requestHeaders"]
-);
+    },
+    { urls: ["https://pos.svia.nl/api/account/"] },
+    ["requestHeaders"]
+  );
+}, {
+  url: [{ hostContains: "svia" }]
+});
