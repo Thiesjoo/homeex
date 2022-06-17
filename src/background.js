8ccdafee53e8cf3f6d828b3e8a4ce4ad2b5e4297
Thies Nieborg <git@thies.dev>
2022-06-17 22:18:33 +0200

    Removed old CI and added toggle for POS syncing

--- src/background.js
+++ src/background.js
@@ -13,27 +13,34 @@ function requestData(url, token) {
 
 chrome.webNavigation.onBeforeNavigate.addListener(function () {
   chrome.webRequest.onSendHeaders.addListener(
-    async function (details) {
-      const correctHeader = details.requestHeaders.find(x => x.name.toLowerCase() === "authorization")
+    function (details) {
+      chrome.storage.sync.get({
+        pos: false
+      }, async function ({ pos }) {
+        if (!pos) {
+          console.log("NOT SYNCING BECAUSE OF CHECKBOX")
+        } else {
+          const correctHeader = details.requestHeaders.find(x => x.name.toLowerCase() === "authorization")
 
-      if (correctHeader) {
-        const token = correctHeader.value.split(" ")?.[1];
-        if (!token) return;
+          if (correctHeader) {
+            const token = correctHeader.value.split(" ")?.[1];
+            if (!token) return;
 
-        try {
-          // Real server has to refresh
-          await fetch("https://auth.thies.dev/auth/refresh/access", {
-            credentials: "include",
-            method: "GET",
-          })
-          requestData("https://auth.thies.dev/api/providers/me/via/pos", token)
-        } catch (e) {
-          console.error("Request to thies.dev failed with error:", e)
+            try {
+              // Real server has to refresh
+              await fetch("https://auth.thies.dev/auth/refresh/access", {
+                credentials: "include",
+                method: "GET",
+              })
+              requestData("https://auth.thies.dev/api/providers/me/via/pos", token)
+            } catch (e) {
+              console.error("Request to thies.dev failed with error:", e)
+            }
+          } else {
+            console.error("How can you have no auth header, but send request to POS")
+          }
         }
-
-      } else {
-        console.error("How can you have no auth header, but send request to POS")
-      }
+      });
     },
     { urls: ["https://pos.svia.nl/api/account/"] },
     ["requestHeaders"]
--- src/options/options.js
+++ src/options/options.js
@@ -1,8 +1,15 @@
 function saveOptions() {
   const version = document.getElementById('version').value;
+  const pos = document.getElementById('enablePos').checked;
+
   chrome.storage.sync.set({
-    version
+    version,
+    pos
   }, function () {
+    const enabled = document.getElementById("enabled")
+    enabled.className = "" + pos;
+    enabled.textContent = pos ? "YES" : "NO"
+
     const status = document.getElementById('status');
     status.textContent = 'Options saved.';
     setTimeout(function () {
@@ -11,13 +18,48 @@ function saveOptions() {
   });
 }
 
-function restoreOptions() {
+async function restoreOptions() {
   chrome.storage.sync.get({
-    version: "production"
-  }, function (items) {
-    document.getElementById('version').value = items.version;
+    version: "production",
+    pos: false
+  }, function ({ version, pos }) {
+    const enabled = document.getElementById("enabled")
+    enabled.className = "" + pos;
+    enabled.textContent = pos ? "YES" : "NO";
+
+    const enablePos = document.getElementById("enablePos")
+    enablePos.checked = pos
+
+    document.getElementById('version').value = version;
   });
+
+
+  // First refresh all of our tokens
+  await fetch("https://auth.thies.dev/auth/refresh/access", {
+    credentials: "include",
+    method: "GET",
+  })
+
+  let userData = await fetch('https://auth.thies.dev/api/users/me', { credentials: 'include' })
+
+  const user = document.getElementById("username")
+  user.textContent = (await userData.json())?.name
+
+  const providerData = await fetch('https://auth.thies.dev/api/providers/me', { credentials: 'include' })
+  const providersAvailable = (await providerData.json())
+  const foundPos = !!providersAvailable.find(x => x.name === "via" && x.id === "pos")
+
+  const syncing = document.getElementById("syncing");
+  syncing.className = "" + foundPos;
+  syncing.textContent = foundPos ? "YES" : "No, please contact admin to enable it for you";
+  if (foundPos) {
+    const enablePos = document.getElementById("enablePos")
+    enablePos.disabled = false
+  } else {
+    chrome.storage.sync.set({ pos: false })
+  }
 }
+
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.getElementById('save').addEventListener('click',
   saveOptions);
